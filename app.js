b513d06718103b9cc8ca1fe6366664ced4c9f803
andresvaron <af.varon@uniandes.edu.co>
2020-08-23 22:20:29 -0500

    procedimiento

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS;
 
 app.use(redirectToHTTPS([/localhost:(\d{4})/], [], 301));
 app.use(express.static(dist));
-
+app.use(express.static("node_modules"));
+app.get("/procedimiento", (request, response) => {
+  response.sendFile(`${__dirname}/Taller 1.pdf`);
+});
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/${dist}/index.html`);
 });
